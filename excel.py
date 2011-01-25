ba1a56edec22ce1238b7248f0842877d1f74c025
Stéphane Bisinger <stephane.bisinger@gmail.com>
2011-01-25 19:13:33 +0000

    Fixed import of excel file

--- excel.py
+++ excel.py
@@ -16,31 +16,56 @@ def importfile( file ):
     return data
 
 def convert_to_db(row):
+    '''Convert Excel data format to a format suitable to the database'''
     r = []
-    r.append(row[7])
-    r.append(row[0])
-    r.append(row[1])
-    (bdate, place) = row[2].split(",")
-    r.append( convert_birth_date(bdate.strip()) )
-    (bplace, bprov) = place.rsplit(" ", 1)
-    r.append(bplace.strip())
-    r.append(bprov.strip())
-    (address, city) = row[3].split(",")
-    r.append(address.strip())
-    (city, prov) = city.split("(")
-    r.append(city.strip())
-    r.append(prov.strip().rstrip(")").strip())
-    r.append(row[4])
-    r.append(row[6])
-    r.append(row[5])
-    r.append(row[9])
-    r.append(row[12])
-    r.append(row[14])
-    r.append( convert_cert_date(row[13] + " " + row[11]) )
+    r.append(None) # ID, automatically generated
+    if row[7].strip() == "no": # Member number
+        r.append(None)
+    else:
+        r.append(row[7])
+    r.append(row[1]) # First name
+    r.append(row[0]) # Last name
+    bdate = ""
+    place = ""
+    if row[2].find(",") == -1:
+        bdate = row[2]
+    else:
+        (bdate, place) = row[2].split(",", 1)
+    r.append( convert_birth_date(bdate.strip()) ) # Birth date
+    place = place.split("(") # Place of birth
+    if len(place) == 1:
+        r.append(place[0].strip()) # City of birth
+        r.append("NN") # Province of birth
+    else:
+        r.append(place[0].strip()) # City of birth
+        r.append( place[1].strip().rstrip(")").strip() ) # Province of birth
+    address = ""
+    city = ""
+    if row[3].find(",") == -1:
+        city = row[3]
+    else:
+        (address, city) = row[3].rsplit(",", 1)
+    r.append(address.strip()) # Address
+    place = city.split("(")
+    if len(place) == 1:
+        r.append(place[0].strip()) # City
+        r.append("NN") # Province
+    else:
+        r.append(place[0].strip()) # City
+        r.append( place[1].strip().rstrip(")").strip() ) # Province
+    r.append(row[4] + " - " + row[6]) # Activity
+    m = row[5].split(" ")
+    r.append(m[0]) # Member Type
+    r.append(m[1]) # Member Year
+    r.append(row[9]) # Telephone
+    r.append(row[12]) # Mobile
+    r.append(row[14]) # E-mail
+    r.append( convert_cert_date(row[13] + " " + row[11]) ) # Certificate validity date
 
     return r
 
 def convert_cert_date(date):
+    '''Convert the date for the certificate in the right format'''
     str_to_month = {
             "gennaio":  "01",
             "febbraio": "02",
@@ -55,13 +80,40 @@ def convert_cert_date(date):
             "novembre": "11",
             "dicembre": "12"
             }
-    (year, month, day) = date.split(" ")
-    if month not in str_to_month:
-        month="01"
+
+    if len(date.strip()) == 0:
+        return None
+
+    d = date.strip().split(" ")
+
+    if len(d) == 1:
+        d.append("01")
+    
+    if len(d) == 2:
+        d.append("01")
+
+    if not d[2].isdigit() and d[1].isdigit():
+        tmp = d[1]
+        d[1] = d[2]
+        d[2] = tmp
+
+    if d[1] not in str_to_month:
+        d[1] ="01"
     else:
-        month = str_to_month[month]
-    return "-".join((year,month,day))
+        d[1] = str_to_month[d[1]]
+
+    if len(d) > 3:
+        print "Data sbagliata? " + date
+
+    d1 = [x.strip() for x in d]
+    return "-".join(d1)
 
 def convert_birth_date(date):
-    (day, month, year) = date.split("-")
-    return "-".join((year,month,day))
+    '''Convert the birth date in the right format'''
+    d = date.split("-")
+    if len(d) == 1:
+        d = date.split("/")
+    if len(d) != 3:
+        print "Data incompleta: " + date
+    d1 = [x.strip() for x in d]
+    return "-".join(d1)
